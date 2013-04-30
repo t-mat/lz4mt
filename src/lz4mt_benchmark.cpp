ddca247363d6f883a93c5c60a0440944092955c5
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2013-04-30 21:08:21 +0900

    refactoring benchmark function
    - log / error mesasges

--- src/lz4mt_benchmark.cpp
+++ src/lz4mt_benchmark.cpp
@@ -21,7 +21,7 @@ TimePoint getTime() {
 }
 
 TimePoint getSyncTime() {
-	auto t0 = getTime();
+	const auto t0 = getTime();
 	for(;;) {
 		auto t = getTime();
 		if(t0 != t) {
@@ -32,7 +32,7 @@ TimePoint getSyncTime() {
 
 double getTimeSpan(const TimePoint& tStart, const TimePoint& tEnd) {
 	using namespace std::chrono;
-	auto dt = duration_cast<seconds>(tEnd - tStart).count();
+	const auto dt = duration_cast<seconds>(tEnd - tStart).count();
 	return static_cast<double>(dt);
 }
 
@@ -55,66 +55,118 @@ int Benchmark::measure(
 ) {
 	using namespace std;
 
+	const auto msgClearLine = [] {
+		fprintf(stderr, "\r%79s\r", "");
+	};
+
+	const auto msgNewline = [] {
+		fprintf(stderr, "\n");
+	};
+
+	const auto msgErrOpening = [](const string& filename) {
+		fprintf(stderr, "Error: problem opening %s\n", filename.c_str());
+	};
+
+	const auto msgLoading = [](const string& filename) {
+		fprintf(stderr, "Loading %s...\r", filename.c_str());
+	};
+
+	const auto msgErrReading = [](const string& filename) {
+		fprintf(stderr, "\nError: problem reading file %s\n"
+				, filename.c_str());
+	};
+
+	const auto msgErrChecksum = []
+		(const string& filename, uint32_t inpHash, uint32_t outHash)
+	{
+		fprintf(stderr
+			, "\n!!! WARNING !!! %14s : Invalid Checksum : %08x != %08x\n"
+			, filename.c_str()
+			, inpHash
+			, outHash
+		);
+	};
+
+	const auto msgReport = []
+		(  int iLoop
+		 , const string& filename
+		 , size_t filesize
+		 , size_t cmpSize
+		 , double minCompressionTime
+		 , double minDecompressionTime
+		)
+	{
+		const auto* fmt = "%1i-%-14.14s :%10.0f ->%10.0f (%6.2f%%)"
+						  ",%7.1f MiB/s, %7.1f MiB/s\r";
+
+		fprintf(stderr, fmt
+			, iLoop
+			, filename.c_str()
+			, (double) (filesize)
+			, (double) (cmpSize)
+			, (double) (cmpSize * 100.0 / filesize)
+			, (double) (filesize / 1024.0 / 1024.0 / minCompressionTime)
+			, (double) (filesize / 1024.0 / 1024.0 / minDecompressionTime)
+		);
+	};
+
 	auto* ctx = &cx;
 	const bool singleThread = 0 != (ctx->mode & LZ4MT_MODE_SEQUENTIAL);
-
-	auto totalFileSize = 0.0;
-	auto totalCompressSize = 0.0;
-	auto totalCompressTime = 0.0;
-	auto totalDecompressTime = 0.0;
-
-	const auto* fmt = "%1i-%-14.14s :%10.0f ->%10.0f (%6.2f%%)"
-					  ",%7.1f MiB/s, %7.1f MiB/s\r";
-
+	size_t totalFileSize = 0;
+	size_t totalCompressSize = 0;
+	double totalCompressTime = 0.0;
+	double totalDecompressTime = 0.0;
 	const auto TIMELOOP = 2.0;	// sec
+
 	for(const auto& filename : files) {
 		vector<char> inpBuf(static_cast<size_t>(getFilesize(filename)));
-		if(!openIstream(ctx, filename)) {
-			std::cerr << "Pb opening " << filename << "\n";
-			return 11;
-		} else {
-			std::cerr << "Loading " << filename << "...        \r";
-			size_t readSize = ctx->read(ctx, inpBuf.data()
+		{
+			if(!openIstream(ctx, filename)) {
+				msgErrOpening(filename);
+				return 11;
+			}
+
+			msgLoading(filename);
+			const size_t readSize = ctx->read(ctx, inpBuf.data()
 									  , static_cast<int>(inpBuf.size()));
 			closeIstream(ctx);
 
-			if(readSize != inpBuf.size()) {
-				std::cerr << "\nError: problem reading file '"
-						  << filename << "' !!    \n";
+			if(inpBuf.size() != readSize) {
+				msgErrReading(filename);
 				return 13;
 			}
 		}
+		msgClearLine();
 
-		const auto inpHash = XXH32(inpBuf.data()
-								   , static_cast<int>(inpBuf.size()), 0);
+		const auto inpHash =
+			XXH32(inpBuf.data(), static_cast<int>(inpBuf.size()), 0);
+		const size_t chunkSize	= (1 << (8 + (2 * sd.bd.blockMaximumSize)));
+		const auto nChunk		= (inpBuf.size() / chunkSize) + 1;
+		const auto maxChunkSize	=
+			static_cast<size_t>(
+				ctx->compressBound(static_cast<int>(chunkSize)));
 
-		vector<char> outBuf;
-		struct ChunkParamters {
+		vector<char> outBuf(nChunk * maxChunkSize);
+
+		struct Chunk {
 			int		id;
-			char*	inpBuffer;
+			char*	inpPtr;
 			size_t	inpSize;
-			char*	outBuffer;
+			char*	outPtr;
 			size_t	outSize;
 			size_t	cmpSize;
 			size_t	decSize;
 		};
-		std::vector<ChunkParamters> chunkParameterss;
 
-		size_t chunkSize	= (1 << (8 + (2 * sd.bd.blockMaximumSize)));
-		auto maxChunkSize	= ctx->compressBound(static_cast<int>(chunkSize));
-		auto nChunk			= (inpBuf.size() / chunkSize) + 1;
-
+		vector<Chunk> chunks(nChunk);
 		{
-			outBuf.resize(nChunk * maxChunkSize);
-			chunkParameterss.resize(nChunk);
-
 			auto r = inpBuf.size();
-			for(auto& e : chunkParameterss) {
-				auto i = static_cast<int>(&e - &chunkParameterss[0]);
+			for(auto& e : chunks) {
+				auto i = static_cast<int>(&e - chunks.data());
 				e.id		= i;
-				e.inpBuffer	= &inpBuf[i * chunkSize];
+				e.inpPtr	= &inpBuf[i * chunkSize];
 				e.inpSize	= r > chunkSize ? chunkSize : r;
-				e.outBuffer	= &outBuf[i * maxChunkSize];
+				e.outPtr	= &outBuf[i * maxChunkSize];
 				e.outSize	= maxChunkSize;
 				e.cmpSize	= 0;
 				e.decSize	= 0;
@@ -122,25 +174,17 @@ int Benchmark::measure(
 			}
 		}
 
-		std::vector<future<void>> futures(chunkParameterss.size());
+		vector<future<void>> futures(chunks.size());
 
-		fprintf(stderr, "\r%79s\r", "");
-
-		const auto filesize = static_cast<double>(inpBuf.size());
-		auto cmpSize = 0.0;
-		auto minCompressionTime   = 10000000.0;
-		auto minDecompressionTime = 10000000.0;
-
-		auto b = [=, &futures, &chunkParameterss] (
-			std::function<void(ChunkParamters*)> fChunk
-		) -> double
+		auto b = [=, &futures, &chunks]
+			(function<void(Chunk*)> fChunk)
 		{
-			auto ms0 = getSyncTime();
-			auto ms1 = ms0;
+			const auto t0 = getSyncTime();
+			auto t1 = t0;
 			int loopCount = 0;
 
-			while(getTimeSpan(ms0, ms1 = getTime()) < TIMELOOP) {
-				for(auto& e : chunkParameterss) {
+			while(getTimeSpan(t0, t1 = getTime()) < TIMELOOP) {
+				for(auto& e : chunks) {
 					futures[e.id] = async(launch::async, fChunk, &e);
 				}
 				for(auto& e : futures) {
@@ -149,105 +193,87 @@ int Benchmark::measure(
 				++loopCount;
 			}
 
-			return getTimeSpan(ms0, ms1) / static_cast<double>(loopCount);
+			return getTimeSpan(t0, t1) / static_cast<double>(loopCount);
 		};
 
-		auto report = [&](int iLoop) {
-			fprintf(stderr, fmt
-				, iLoop
-				, filename.c_str()
-				, filesize
-				, cmpSize
-				, cmpSize / filesize * 100.0
-				, filesize / 1024.0 / 1024.0 / minCompressionTime
-				, filesize / 1024.0 / 1024.0 / minDecompressionTime
-			);
-		};
-
+		auto minCmpTime = 10000000.0;
+		auto minDecTime = 10000000.0;
+		size_t cmpSize = 0;
 		for(int iLoop = 1, nLoop = nIter; iLoop <= nLoop; ++iLoop) {
-			//	compression
+			msgReport(iLoop, filename, inpBuf.size()
+					  , cmpSize, minCmpTime, minDecTime);
+
+			// compression
 			for(size_t i = 0; i < outBuf.size(); ++i) {
 				outBuf[i] = static_cast<char>(i);
 			}
-			auto tComp = b(
-				[ctx, singleThread, &futures] (ChunkParamters* cp) {
+			auto cmpTime = b(
+				[ctx, singleThread, &futures] (Chunk* cp) {
 					if(singleThread && cp->id > 0) {
 						futures[cp->id-1].wait();
 					}
 					cp->cmpSize = ctx->compress(
-						  cp->inpBuffer
-						, cp->outBuffer
+						  cp->inpPtr
+						, cp->outPtr
 						, static_cast<int>(cp->inpSize)
 						, static_cast<int>(cp->outSize)
 					);
 				}
 			);
-			minCompressionTime = min(minCompressionTime, tComp);
+			minCmpTime = min(minCmpTime, cmpTime);
 
-			cmpSize = 0.0;
-			for(auto& c : chunkParameterss) {
-				cmpSize += static_cast<double>(c.cmpSize);
+			if(1 == iLoop) {
+				cmpSize = 0;
+				for(auto& c : chunks) {
+					cmpSize += c.cmpSize;
+				}
 			}
 
-			report(iLoop);
+			msgReport(iLoop, filename, inpBuf.size()
+					  , cmpSize, minCmpTime, minDecTime);
 
-			//	decompression
+			// decompression
 			for(auto& e : inpBuf) {
 				e = 0;
 			}
-			auto tDecomp = b(
-				[ctx, singleThread, &futures] (ChunkParamters* cp) {
+			auto decTime = b(
+				[ctx, singleThread, &futures] (Chunk* cp) {
 					if(singleThread && cp->id > 0) {
 						futures[cp->id-1].wait();
 					}
 					cp->decSize = ctx->decompress(
-						  cp->outBuffer
-						, cp->inpBuffer
+						  cp->outPtr
+						, cp->inpPtr
 						, static_cast<int>(cp->cmpSize)
 						, static_cast<int>(cp->inpSize)
 					);
 				}
 			);
-			minDecompressionTime = min(minDecompressionTime, tDecomp);
+			minDecTime = min(minDecTime, decTime);
 
-			report(iLoop);
+			msgReport(iLoop, filename, inpBuf.size()
+					  , cmpSize, minCmpTime, minDecTime);
 
-			const auto outHash = XXH32(
-				  inpBuf.data()
-				, static_cast<int>(inpBuf.size())
-				, 0
-			);
+			const auto outHash =
+				XXH32(inpBuf.data(), static_cast<int>(inpBuf.size()), 0);
 
 			if(inpHash != outHash) {
-		 		fprintf(stderr
-					, "\n!!! WARNING !!! "
-					  "%14s : Invalid Checksum : %x != %x\n"
-					, filename.c_str()
-					, inpHash
-					, outHash
-				);
+				msgErrChecksum(filename, inpHash, outHash);
 				break;
 			}
 		}
-		fprintf(stderr, "\n");
+		msgNewline();
 
-		totalFileSize += filesize;
+		totalFileSize += inpBuf.size();
 		totalCompressSize += cmpSize;
-		totalCompressTime += minCompressionTime;
-		totalDecompressTime += minDecompressionTime;
+		totalCompressTime += minCmpTime;
+		totalDecompressTime += minDecTime;
 	}
 
 	if(!files.empty()) {
-		fprintf(stderr, fmt
-			, 0
-			, "  TOTAL"
-			, totalFileSize
-			, totalCompressSize
-			, totalCompressSize / totalFileSize * 100.0
-			, totalFileSize / 1024.0 / 1024.0 / totalCompressTime
-			, totalFileSize / 1024.0 / 1024.0 / totalDecompressTime
-		);
-		fprintf(stderr, "\n");
+		msgReport(0, "  TOTAL", totalFileSize, totalCompressSize
+				  , totalCompressTime, totalDecompressTime);
+		msgNewline();
 	}
 
 	return 0;
