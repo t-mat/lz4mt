c23c9b518983b27f2d44321d2075c144b449a92a
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2013-06-19 14:25:03 +0900

    Add thread pool class

---
+++ src/lz4mt_threadpool.cpp
@@ -0,0 +1,74 @@
+#include <cassert>
+#include <atomic>
+#include <future>
+#include <mutex>
+#include <queue>
+#include <vector>
+#include "lz4mt_threadpool.h"
+#include "lz4mt_compat.h"
+
+namespace Lz4Mt {
+
+// https://github.com/progschj/ThreadPool
+
+ThreadPool::ThreadPool(size_t nThread)
+	: stop()
+	, mut()
+	, cond()
+	, tasks()
+	, threads()
+{
+	stop.store(false);
+
+	if(0 == nThread) {
+		nThread = Lz4Mt::getHardwareConcurrency();
+		assert(nThread != 0);
+		if(0 == nThread) {
+			nThread = 1;
+		}
+	}
+
+	for(int i = 0; i < static_cast<int>(nThread); ++i) {
+		// @NOTE: I want to set some kind of affinity mask here,
+		// but C++11 is not support any kind of affinity mask.
+		threads.push_back(std::thread(
+			[this, i] {
+				for(;;) {
+					Lock lock(mut);
+					while(!stop.load() && tasks.empty()) {
+						cond.wait(lock);
+					}
+					if(stop.load() && tasks.empty()) {
+						break;
+					}
+					const auto t = std::move(tasks.front());
+					tasks.pop();
+					lock.unlock();
+					t(i);
+				}
+			}
+		));
+	}
+}
+
+
+ThreadPool::~ThreadPool() {
+	joinAll();
+}
+
+
+void ThreadPool::joinAll() {
+	{
+		Lock lock(mut);
+		stop.store(true);
+	}
+	cond.notify_all();
+
+	for(auto& e : threads) {
+		if(e.joinable()) {
+			e.join();
+		}
+	}
+}
+
+} // namespace Lz4Mt
---
+++ src/lz4mt_threadpool.h
@@ -0,0 +1,35 @@
+#ifndef LZ4MT_THREADPOOL_H
+#define LZ4MT_THREADPOOL_H
+
+namespace Lz4Mt {
+
+class ThreadPool {
+public:
+	ThreadPool(size_t nThread = 0);
+	~ThreadPool();
+	void joinAll();
+
+	template<class F>
+	void enqueue(F&& f) {
+		{
+			Lock lock(mut);
+			tasks.push(std::move(f));
+		}
+		cond.notify_one();
+	}
+
+private:
+    typedef std::unique_lock<std::mutex> Lock;
+    typedef std::function<void(int)> Task;
+
+    std::atomic<bool> stop;
+    mutable std::mutex mut;
+    std::condition_variable cond;
+
+    std::queue<Task> tasks;
+	std::vector<std::thread> threads;
+};
+
+} // namespace Lz4Mt
+
+#endif // LZ4MT_THREADPOOL_H
